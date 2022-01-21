250e7fa1186d469f5046cb820a154e4f8b2aeb04
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-01-21 15:57:52 +0300

    Pushed file to github

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let hello = () => {
+    let hoursDisplay = document.getElementById("clock-hours");
+    let minutesDisplay = document.getElementById("clock-minutes");
+    let secondsDisplay = document.getElementById("clock-seconds");
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    hoursDisplay.innerText = `${hours}`;
+    minutesDisplay.innerText = `${minutes}`;
+    secondsDisplay.innerText = `${seconds}`;
+    setInterval(hello, 1000);
+};
