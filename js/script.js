6ec6dae2e7494c5c3a4fedcebe08d4d01c5f1152
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-01-23 12:11:29 +0300

    Jumped to previous code

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ let hello = () => {
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
- 
+
     hoursDisplay.innerText = `${hours}`;
     minutesDisplay.innerText = `${minutes}`;
     secondsDisplay.innerText = `${seconds}`;
-    
+
     setInterval(hello, 1000);
 };
