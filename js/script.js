3add83038ec50e52f1f8cf643924045c49674bc9
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-01-23 12:09:49 +0300

    Went back to the previous code

--- js/script.js
+++ js/script.js
@@ -6,16 +6,10 @@ let hello = () => {
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
-    if (seconds <= 9) {
-        secondsDisplay.innerText = `0${seconds}`;
-    } else if (minutes <= 9) {
-        minutesDisplay.innerText = `0${minutes}`;
-    } else if (hours <= 9) {
-        minutesDisplay.innerText = `0${hours}`;
-    } else {
-        hoursDisplay.innerText = `${hours}`;
-        minutesDisplay.innerText = `${minutes}`;
-        secondsDisplay.innerText = `${seconds}`;
-    }
+ 
+    hoursDisplay.innerText = `${hours}`;
+    minutesDisplay.innerText = `${minutes}`;
+    secondsDisplay.innerText = `${seconds}`;
+    
     setInterval(hello, 1000);
 };
